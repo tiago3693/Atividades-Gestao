19716a5e07de99bfd94b47200c87b70908947139
Tiago de Britto Antunes - 42025558 <42025558@ulife.com.br>
2022-04-26 21:25:48 -0300

    pratica 09

---
+++ Gestão e qualidade de software (segunda e terça)/pratica_09/src/pratica_09/PraticaMain.java
@@ -0,0 +1,33 @@
+package pratica_09;
+
+import java.util.Scanner;
+
+public class PraticaMain {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Selecione a questão: ");
+		int num1 = scan.nextInt();
+		System.out.println();
+		switch (num1) {
+		case 1:
+			Pratica_09_1.pratica();
+			break;
+		case 2:
+			Pratica_09_2.pratica();
+			break;
+		case 3:
+			Pratica_09_3.pratica();
+			break;
+		case 4:
+			Pratica_09_4.pratica();
+			break;
+		}
+
+		scan.close();
+
+	}
+
+}
